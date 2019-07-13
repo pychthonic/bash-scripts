2f109f31d21a5554ff030cf5c3f3d255291bf99c
Stephen Wolfe <fridgewolf@protonmail.com>
2019-07-13 09:42:56 -0400

    made the logic more precise

--- inPATH.py
+++ inPATH.py
@@ -12,18 +12,21 @@ def in_path(file_name):
     found = False
     if file_name[0] == '/':
         if os.access(file_name, os.X_OK):
-            print("File is executable but not found in PATH variable directory list.")
+            print("Filename refers to an executable but not found in PATH variable directory list.")
         else:
-            print("File not executable and not in PATH variable directory list.")
-    for directory in path_dir_list:
-        full_file_name = os.path.join(directory, file_name)
-        if os.path.isfile(full_file_name):
-            print(f"Found in PATH environment variable directory: {full_file_name}")
-            found = True
-            if os.access(full_file_name, os.X_OK):
-                print(f"{full_file_name} is executable.")
-    if not found:
-        print("{file_name} not found in PATH environment variable directories.")
+            print("Filename does not refer to an executable and not in PATH variable directory list.")
+    else:
+        for directory in path_dir_list:
+            full_file_name = os.path.join(directory, file_name)
+            if os.path.isfile(full_file_name):
+                print(f"Found in PATH environment variable directory: {full_file_name}")
+                found = True
+                if os.access(full_file_name, os.X_OK):
+                    print(f"{full_file_name} is executable.")
+                else:
+                    print(f"{full_file_name} is not executable.")
+        if not found:
+            print(f"{file_name} not found in PATH environment variable directories. Executable status unknown since the file could not be located.")
 
 
 if __name__ == '__main__':  
