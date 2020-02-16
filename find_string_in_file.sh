39f9d76ec5450792f9f51fc28d78c2f9f56bb5c8
pychthonic <fridgewolf@protonmail.com>
2020-02-15 23:13:43 -0500

    script now installs pcregrep dependency on RedHat- and Debian-based systems

--- find_string_in_file.sh
+++ find_string_in_file.sh
@@ -1,14 +1,53 @@
-#!/bin/bash
+# !/bin/bash
 # arg1: filename_to_parse
 # arg2: search_string
-# Simple bash script that counts how many times a string
+# Bash script that counts how many times a string
 # is found in a file, including when it falls across
 # multiple lines.
+# Depends on pcregrep -- if it's not installed on
+# your system, you have the option of letting the
+# bash script installing it for you.
 
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <file-to-search> <string-to-search-for>"
     exit    
+fi
+
+hash pcregrep &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Dependancy not found: pcregrep."
+    yes_or_no=0
+    while [[ $yes_or_no != "y" ]] && [[ $yes_or_no != "n" ]]; do
+        echo "Install pcregrep with package manager? (y)es or (n)o: "
+        read yes_or_no
+    done 
+    if [ $yes_or_no == "y" ]; then
+        OS=$(uname) 
+        if [ $OS == "Linux" ]; then
+            if [ -f /etc/redhat-release ]; then 
+                DistroBasedOn="RedHat"
+            elif [ -f /etc/debian_version ]; then
+                DistroBasedOn="Debian"
+            else
+                echo "Failed to determine Linux distro. Exiting."
+                exit
+            fi
+        else
+            echo "Sorry, this bash script was written for Redhat- and Debian- based Linux distros. Exiting."
+            echo
+            exit
+        fi
+        if [ $DistroBasedOn == "RedHat" ]; then
+            sudo yum install pcregrep
+        elif [ $DistroBasedOn == "Debian" ]; then
+            sudo apt install pcregrep
+        fi 
+
+    else
+        exit
+    fi
 fi
 
 file_to_search=$1
